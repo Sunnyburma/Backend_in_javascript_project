8661ad98e9da18d4e4ede557d9d4dc5b639806a7
sunny burman <sunnyb142@gmail.com>
2025-01-06 22:47:30 +0530

    correction on name

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error{
             statusCode,
             message="something is Error",
             errors = [],
-            statck=""
+            stack=""
         ){
             super(message)
             this.statusCode = statusCode
@@ -13,7 +13,7 @@ class ApiError extends Error{
             this.errors = this.errors
 
             if(statck){
-                this.statck = statck;
+                this.stack = stack;
             }else{
                 Error.captureStackTrace(this, this.constructor)
             }
